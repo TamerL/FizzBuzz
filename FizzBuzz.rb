59c107c78cf339c1c55e5d14686f455fe22d4ec1
tamerl <eng.tamereid@gmail.com>
2020-01-08 15:21:13 +1100

    Refactoring the code

--- FizzBuzz.rb
+++ FizzBuzz.rb
@@ -1,6 +1,6 @@
 (1..100).each do |x|
   case x
-  when -> (x) {x % 3 == 0 && x % 5==0} then puts "FizzBuzz"
+  when -> (x) {x % 15 == 0} then puts "FizzBuzz"
   when -> (x) {x % 3 == 0} then puts "Fizz"
   when -> (x) {x % 5 == 0} then puts "Buzz"
   else puts x
